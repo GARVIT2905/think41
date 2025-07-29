4740fecc20818520db72ea9aab5fa523456a2f0c
Garvit sahu <101084807+GARVIT2905@users.noreply.github.com>
2025-07-29 12:26:48 +0530

    Update dependencyRequestDto.java

--- dependencyRequestDto.java
+++ dependencyRequestDto.java
@@ -1,11 +1,1 @@
-package com.example.workflow.dto;
 
-import jakarta.validation.constraints.NotBlankpublic class DependencyRequestDto {
-    @NotBlank
-    private String stepStrId;
-
-    @NotBlank
-    private String prerequisiteStepStrId;
-
-    
-}
